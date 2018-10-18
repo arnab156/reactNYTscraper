5728ba562a4dc15bf49c8c497d8ad8c632b9d48a
arnab majumdar <arnab156@gmail.com>
2018-10-18 10:43:41 -0500

    proxy setting changed

--- client/src/utils/api/API.js
+++ client/src/utils/api/API.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 export default {
   getArticles: function() {
-    return axios.get("/api/articles");
+    return axios.get("/api/articles/");
   },
   deleteArticle: function(id) {
     return axios.delete("/api/articles/" + id);
   },
   saveArticle: function(articleData) {
+    console.log("inside the save article in API js");
     return axios.post("/api/articles", articleData);
   }
 };
